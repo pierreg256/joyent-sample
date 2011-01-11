5ab3914da4a6175fab3465134edc4831c2ea6544
Pierreg256 <pierre.gilot@me.com>
2011-01-11 11:57:08 +0100

    premier fichier server

--- server.js
+++ server.js
@@ -3,5 +3,8 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'content-type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('My Own Private Hello World\n');
 }).listen(80);
+
+console.log("service demarre");
+
